e569427be180e27534d0b10a023062cd3cb34c0c
Aaron <kahanam24@gmail.com>
2019-02-28 21:08:49 -0600

    1

--- plugin2/script.js
+++ plugin2/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   elDoc.className = elDoc.className.replace(/(^|\s)no-js(\s|$)/, '$1')
   //Show info in the console
   console.group('Welcome');
-    console.log('%cAaron Kahan', 'color:#264e36; font-size:30px; font-weight:bold;');
+    console.log('%cHi', 'color:#264e36; font-size:30px; font-weight:bold;');
     console.log('%cjQuery version:', 'color:#DAD3AF; font-size:30px; font-weight:bold;', $.fn.jquery);
   console.groupEnd();
   (function() {
